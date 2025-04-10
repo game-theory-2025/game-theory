22b70c487f29b2895627210536886565c5ff735e
0x000Saji <skm.kiani@gmail.com>
2025-04-10 13:39:35 +0330

    add gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Layout from './components/Layout';
 
 function App () {
   return (
-    <Router>
+    <Router basename='/game-theory/'>
       <div>
         <Routes>
           <Route path="/" element={<Layout> <Home /> </Layout>} />
